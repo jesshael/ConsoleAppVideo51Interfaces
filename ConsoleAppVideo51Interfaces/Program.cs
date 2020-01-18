eb1c2a9c5df385c48834233ac718420257a5d927
Jesshael Cabrera Pedroza <jesshael.cabrera@logrand.com>
2020-01-17 21:20:09 -0600

    Add project files.

---
+++ ConsoleAppVideo51Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppVideo51Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
